f257dd587546040d74d15d7ae086816072ac28ba
Emelie Hansson <emeliehanssonn@hotmail.se>
2024-01-10 15:03:33 +0100

    sign in done

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -20,6 +20,7 @@ import { Ionicons } from "@expo/vector-icons"; //import { AuthContext } from "..
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import SvgComponent from "../Images/SvgComponent";
 import Svg, { SvgProps, G, Path } from "react-native-svg";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 interface Props {
   navigation: any;
@@ -109,7 +110,7 @@ export const SignInScreen = ({ navigation }: Props) => {
       </View>
       {/* <SvgComponent /> */}
       <View style={styles.inputContainer}>
-        <Svg
+        {/* <Svg
           width={395.342}
           height={569.611}
           style={{ position: "absolute", top: -55, zIndex: 1 }}
@@ -117,52 +118,63 @@ export const SignInScreen = ({ navigation }: Props) => {
           <G fill="#fff">
             <Path d="M390.279 98.592s3.807-37.773 3.074-65.075c-.178-6.63-.638-13.36-.009-19.154.365-3.329 3.122-7.123-.003-7.514-11.03-1.388-22.874 18.685-52.528 26.692S239.991 54.569 151.025 53.627 17.312 43.157 1.279 48.947c.593 28.947.023 79.16.023 79.16" />
             <Path d="M2.342 80.611h393v489h-393z" />
-          </G>
-          <View
-            style={{
-              marginTop: 80,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <View style={styles.input}>
-              <Ionicons name="person" size={24} color="black" />
-              <TextInput
-                onChangeText={setMail}
-                defaultValue={"test@hotmail.se"}
-                placeholder="Email"
-                placeholderTextColor={"#636363"}
-                style={styles.inputText}
-                autoComplete="email"
-              />
-            </View>
-            <View style={styles.input}>
-              <Ionicons name="lock-closed-outline" size={24} color="black" />
-              <TextInput
-                secureTextEntry={true}
-                onChangeText={setPassword}
-                defaultValue={password}
-                placeholder="Password"
-                placeholderTextColor={"#636363"}
-                style={styles.inputText}
-              />
-            </View>
+          </G> */}
+        {/* <View
+          style={{
+            marginTop: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        > */}
+        <View style={styles.input}>
+          <Ionicons name="person" size={24} color="black" />
+          <TextInput
+            onChangeText={setMail}
+            defaultValue={"test@hotmail.se"}
+            placeholder="Email"
+            placeholderTextColor={"#636363"}
+            style={styles.inputText}
+            autoComplete="email"
+          />
+        </View>
+        <View style={styles.input}>
+          <Ionicons name="lock-closed-outline" size={24} color="black" />
+          <TextInput
+            secureTextEntry={true}
+            onChangeText={setPassword}
+            defaultValue={password}
+            placeholder="Password"
+            placeholderTextColor={"#636363"}
+            style={styles.inputText}
+          />
+        </View>
+        <View style={{ width: "100%" }}>
+          <TouchableOpacity onPress={signIn} style={styles.SignInButton}>
+            <LinearGradient
+              colors={["#BBE29D", "#D3EFBE", "#BBE29D"]}
+              style={{
+                paddingHorizontal: 10,
+                paddingVertical: 15,
+                borderRadius: 5,
+                elevation: 3,
+              }}
+            >
+              <Text style={styles.signInButtonText}>Sign in</Text>
+            </LinearGradient>
+          </TouchableOpacity>
 
-            <Pressable onPress={signIn}>
-              <Text style={styles.SignInButton}>Sign in</Text>
-            </Pressable>
+          <Pressable onPress={() => navigation.navigate("Sign up")}>
+            <Text style={styles.SignUpButton}>Register</Text>
+          </Pressable>
 
-            <Pressable onPress={() => navigation.navigate("Sign up")}>
-              <Text style={styles.SignUpButton}>Register</Text>
-            </Pressable>
-
-            <Pressable onPress={() => Alert.alert("Glömt lösenord")}>
-              <Text style={styles.forgotPasswordButton}>Forgot password?</Text>
-            </Pressable>
-          </View>
-        </Svg>
+          <Pressable onPress={() => Alert.alert("Glömt lösenord")}>
+            <Text style={styles.forgotPasswordButton}>Forgot password?</Text>
+          </Pressable>
+        </View>
       </View>
+      {/* </Svg> */}
     </View>
+    // </View>
     //   <SafeAreaView>
     //  </SafeAreaView>
     //</ImageBackground>
@@ -180,55 +192,52 @@ const styles = StyleSheet.create({
   inputContainer: {
     flex: 2,
     zIndex: 1,
+    backgroundColor: "#F6F6F6",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 40,
   },
   input: {
     flexDirection: "row",
     borderRadius: 5,
     padding: 10,
-    backgroundColor: "#f5f5f5",
-    zIndex: 1,
+    backgroundColor: "#fff",
+    marginBottom: 10,
+    width: "100%",
   },
   inputText: {
     marginLeft: 13,
     fontSize: 18,
   },
-  title: {
-    fontSize: 32,
-    fontWeight: "bold",
-    letterSpacing: 0.25,
-    color: "white",
-    marginBottom: 50,
-  },
   forgotPasswordButton: {
     fontSize: 16,
     fontWeight: "bold",
     letterSpacing: 0.25,
-    color: "white",
-    marginTop: 50,
+    color: "black",
+    marginTop: 15,
+    textAlign: "center",
   },
   SignInButton: {
-    backgroundColor: "#0a2121",
-    width: 300,
-    textAlign: "center",
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 5,
-    elevation: 3,
+    width: "100%",
+    shadowColor: "#080808",
+    shadowOffset: { width: -1, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 4,
+  },
+  signInButtonText: {
     fontSize: 16,
     lineHeight: 21,
     fontWeight: "bold",
     letterSpacing: 0.25,
-    color: "white",
-    marginTop: 20,
-    borderWidth: 2,
-    borderColor: "#0a2121",
+    textAlign: "center",
   },
   SignUpButton: {
     backgroundColor: "white",
-    width: 300,
+    width: "100%",
     textAlign: "center",
-    paddingVertical: 12,
-    paddingHorizontal: 32,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
     borderRadius: 5,
     elevation: 3,
     fontSize: 16,
@@ -236,13 +245,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     letterSpacing: 0.25,
     color: "black",
-    marginTop: 20,
+    marginTop: 10,
     borderWidth: 2,
-    borderColor: "#123b3b",
-  },
-  button: {
-    width: 300,
-    height: 50,
-    backgroundColor: "pink",
+    borderColor: "#597D3E",
   },
 });
