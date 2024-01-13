e5d60e30437efa850081931b5f4615dab5e71d63
Emelie <78534885+MissPixxie@users.noreply.github.com>
2024-01-13 18:31:02 +0100

    more ducking drawer styling

--- src/navigation/CustomDrawer.tsx
+++ src/navigation/CustomDrawer.tsx
@@ -1,6 +1,13 @@
 import { DrawerContentScrollView } from "@react-navigation/drawer";
-import React, { useContext } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useContext, useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Switch,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { AuthContext, defaultContextState } from "../context/AuthContext";
 
 //CONTEXT
@@ -8,7 +15,13 @@ import { CustomButton } from "../components/CustomButton";
 import { ThemeContext } from "../context/ThemeContext";
 
 //ICONS
-import { FontAwesome, Ionicons, Octicons } from "@expo/vector-icons";
+import {
+  FontAwesome,
+  Ionicons,
+  Octicons,
+  Entypo,
+  Feather,
+} from "@expo/vector-icons";
 import { Image } from "expo-image";
 import { LinearGradient } from "expo-linear-gradient";
 import Svg, { Ellipse, G } from "react-native-svg";
@@ -20,6 +33,11 @@ interface Props {
 export const CustomDrawer = ({ navigation }: Props) => {
   const { state, setState } = useContext(AuthContext);
   const { theme, toggleTheme } = useContext(ThemeContext);
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => {
+    setIsEnabled((previousState) => !previousState);
+    toggleTheme();
+  };
 
   const { colors } = theme;
   const { drawer } = colors;
@@ -39,6 +57,13 @@ export const CustomDrawer = ({ navigation }: Props) => {
     links: {
       marginBottom: 25,
     },
+    toggleThemeButton: {
+      alignSelf: "center",
+      transform:
+        Platform.OS === "ios"
+          ? [{ scaleX: 0.9 }, { scaleY: 0.9 }]
+          : [{ scaleX: 1.2 }, { scaleY: 1.2 }],
+    },
   });
 
   return (
@@ -49,142 +74,182 @@ export const CustomDrawer = ({ navigation }: Props) => {
         backgroundColor: "white",
       }}
     >
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "white",
-          justifyContent: "center",
-          alignItems: "center",
-          borderBottomWidth: 2,
-          borderBottomColor: "#EBF9E0",
-        }}
+      <LinearGradient
+        start={theme.dark ? { x: -3, y: 0 } : { x: 0, y: 0 }}
+        end={theme.dark ? { x: 1, y: 1 } : { x: 2, y: 1 }}
+        colors={
+          theme.dark ? ["#3D3D3D", colors.background] : ["#fff", "#BBE29D"]
+        }
+        style={{ flex: 1 }}
       >
-        <Image
-          source={require("../Images/avatar.jpg")}
-          style={{ height: 120, width: 120, borderRadius: 60 }}
-        />
-      </View>
-      <View
-        style={{ paddingTop: 20, flex: 4, backgroundColor: colors.background }}
-      >
-        <Svg
+        <View
           style={{
-            position: "absolute",
-            zIndex: -1,
+            backgroundColor: "white",
+            justifyContent: "center",
+            alignItems: "center",
+            borderBottomWidth: 2,
+            borderBottomColor: "#EBF9E0",
           }}
         >
-          <G transform="translate(34.427 -197.218)">
-            <Ellipse
-              cx={189.5}
-              cy={332.5}
-              fill={theme.dark ? "#F3FFEB" : "#FFF"}
-              fillOpacity={theme.dark ? 0.4 : 1}
-              rx={189.5}
-              ry={332.5}
-              transform="rotate(12.04 -857.228 859.75)"
-            />
-            <Ellipse
-              cx={183.572}
-              cy={323.196}
-              fill="#EBF9E0"
-              fillOpacity={theme.dark ? 0.3 : 0.9}
-              opacity={0.61}
-              rx={183.572}
-              ry={323.196}
-              transform="rotate(-5 3651.248 -191.369)"
-            />
-            <Ellipse
-              cx={189.5}
-              cy={332.5}
-              fill={theme.dark ? "#DBF1CA" : "#86AE69"}
-              fillOpacity={theme.dark ? 0.4 : 0.1}
-              opacity={0.76}
-              rx={189.5}
-              ry={332.5}
-              transform="rotate(-19.01 1470.716 351.908)"
-            />
-          </G>
-        </Svg>
-        <View style={styles.links}>
-          <View style={styles.titleContainer}>
-            <Ionicons name="person" size={20} color={colors.text} />
-            <Text style={styles.title}> Account </Text>
-          </View>
-          <CustomButton
-            title="Profile"
-            fontSize={16}
-            onPress={() => {}}
-            bgColor={colors.secondaryLight}
-          />
-          <CustomButton
-            title="Privacy settings"
-            fontSize={16}
-            onPress={() => {}}
-            bgColor={colors.secondaryLight}
-          />
-          <CustomButton
-            title="Notifications"
-            fontSize={16}
-            onPress={() => {}}
-            bgColor={colors.secondaryLight}
-          />
-          <CustomButton
-            title="Theme"
-            fontSize={16}
-            onPress={toggleTheme}
-            bgColor={colors.secondaryLight}
-          />
-          <View style={styles.titleContainer}>
-            <FontAwesome name="question-circle" size={20} color={colors.text} />
-            <Text style={styles.title}> Help </Text>
-          </View>
-          <CustomButton
-            title="About Furbooks"
-            fontSize={16}
-            onPress={() => {}}
-            bgColor={colors.secondaryLight}
-          />
-          <CustomButton
-            title="Contact"
-            fontSize={16}
-            onPress={() => {}}
-            bgColor={colors.secondaryLight}
-          />
-          <CustomButton
-            title="Privacy policy"
-            fontSize={16}
-            onPress={() => {}}
-            bgColor={colors.secondaryLight}
+          <Image
+            source={require("../Images/avatar.jpg")}
+            style={{ height: 120, width: 120, borderRadius: 60 }}
           />
         </View>
-        <View>
-          <LinearGradient
-            colors={
-              theme.dark ? ["#BBE29D", "#E1F2D4"] : ["white", colors.secondary]
-            }
+        <View style={{ paddingTop: 20, flex: 1 }}>
+          <Svg
             style={{
-              justifyContent: "center",
-              alignItems: "center",
-              paddingVertical: 20,
+              position: "absolute",
+              zIndex: -1,
             }}
           >
-            <TouchableOpacity
-              onPress={() => {
-                setState(defaultContextState);
-                navigation.closeDrawer();
-              }}
-              style={{
-                flexDirection: "row",
-              }}
-            >
-              <Text style={{ fontSize: 18, marginRight: 25, color: "black" }}>
-                Sign out
-              </Text>
-              <Octicons name="sign-out" size={22} color="black" />
-            </TouchableOpacity>
-          </LinearGradient>
+            <G transform="translate(34.427 -197.218)">
+              <Ellipse
+                cx={189.5}
+                cy={332.5}
+                fill={theme.dark ? "#F3FFEB" : "#FFF"}
+                fillOpacity={theme.dark ? 0.4 : 1}
+                rx={189.5}
+                ry={332.5}
+                transform="rotate(12.04 -857.228 859.75)"
+              />
+              <Ellipse
+                cx={183.572}
+                cy={323.196}
+                fill="#EBF9E0"
+                fillOpacity={theme.dark ? 0.3 : 0.9}
+                opacity={0.61}
+                rx={183.572}
+                ry={323.196}
+                transform="rotate(-5 3651.248 -191.369)"
+              />
+              <Ellipse
+                cx={189.5}
+                cy={332.5}
+                fill={theme.dark ? "#DBF1CA" : "#86AE69"}
+                fillOpacity={theme.dark ? 0.4 : 0.1}
+                opacity={0.76}
+                rx={189.5}
+                ry={332.5}
+                transform="rotate(-19.01 1470.716 351.908)"
+              />
+            </G>
+          </Svg>
+          <View style={{ flex: 1, justifyContent: "space-between" }}>
+            <View style={styles.links}>
+              <View style={styles.titleContainer}>
+                <Ionicons name="person" size={20} color={colors.text} />
+                <Text style={styles.title}> Account </Text>
+              </View>
+              <CustomButton
+                title="Profile"
+                fontSize={16}
+                onPress={() => {}}
+                bgColor={colors.secondaryLight}
+              />
+              <CustomButton
+                title="Privacy settings"
+                fontSize={16}
+                onPress={() => {}}
+                bgColor={colors.secondaryLight}
+              />
+              <CustomButton
+                title="Notifications"
+                fontSize={16}
+                onPress={() => {}}
+                bgColor={colors.secondaryLight}
+              />
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
+                  alignItems: "center",
+                  paddingHorizontal: 40,
+                  marginVertical: Platform.OS === "ios" ? 20 : 0,
+                }}
+              >
+                <Entypo
+                  name="light-up"
+                  size={18}
+                  color={theme.dark ? colors.text : colors.text}
+                />
+                <Switch
+                  trackColor={{ false: "#BBE29D", true: "#616161" }}
+                  thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
+                  onValueChange={toggleSwitch}
+                  value={isEnabled}
+                  style={styles.toggleThemeButton}
+                />
+                <Feather
+                  name="moon"
+                  size={18}
+                  color={theme.dark ? colors.text : colors.text}
+                />
+              </View>
+              <View style={styles.titleContainer}>
+                <FontAwesome
+                  name="question-circle"
+                  size={20}
+                  color={colors.text}
+                />
+                <Text style={styles.title}> Help </Text>
+              </View>
+              <CustomButton
+                title="About Furbooks"
+                fontSize={16}
+                onPress={() => {}}
+                bgColor={colors.secondaryLight}
+              />
+              <CustomButton
+                title="Contact"
+                fontSize={16}
+                onPress={() => {}}
+                bgColor={colors.secondaryLight}
+              />
+              <CustomButton
+                title="Privacy policy"
+                fontSize={16}
+                onPress={() => {}}
+                bgColor={colors.secondaryLight}
+              />
+            </View>
+
+            {/* SIGN OUT BUTTON */}
+            <View>
+              <LinearGradient
+                colors={
+                  theme.dark
+                    ? ["#BBE29D", "#E1F2D4"]
+                    : ["white", colors.secondary]
+                }
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  paddingVertical: 20,
+                }}
+              >
+                <TouchableOpacity
+                  onPress={() => {
+                    setState(defaultContextState);
+                    navigation.closeDrawer();
+                  }}
+                  style={{
+                    flexDirection: "row",
+                  }}
+                >
+                  <Text
+                    style={{ fontSize: 18, marginRight: 25, color: "black" }}
+                  >
+                    Sign out
+                  </Text>
+                  <Octicons name="sign-out" size={22} color="black" />
+                </TouchableOpacity>
+              </LinearGradient>
+            </View>
+            {/* SIGN OUT BUTTON END */}
+          </View>
         </View>
-      </View>
+      </LinearGradient>
 
       {/* <Switch
         trackColor={newTheme === "light" ? "#767577" : "#81ff83"}
